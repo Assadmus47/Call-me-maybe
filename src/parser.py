6efb127c7d5dcfbf1f1c0a7301d79f4840adc7d6
Assadmus47 <assadkac47@gmail.com>
2026-06-25 05:10:53 +0200

    feat: add json parsers with error handling

--- src/parser.py
+++ src/parser.py
@@ -3,14 +3,26 @@ from models import Function, FunctionCallingTest
 
 
 def parse_function_file(filename: str) -> list[Function]:
-    with open(filename, "r") as f:
-        data = json.load(f)
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
 
-    return [Function(**item) for item in data]
+        return [Function(**item) for item in data]
+
+    except json.JSONDecodeError:
+        raise SystemExit(f"ERROR: Invalid JSON in file: {filename}")
+    except FileNotFoundError:
+        raise SystemExit("ERROR: File Not Found.")
 
 
 def parse_calling_function_file(filename: str) -> list[FunctionCallingTest]:
-    with open(filename, "r") as f:
-        data = json.load(f)
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
 
-    return [FunctionCallingTest(**item) for item in data]
+        return [FunctionCallingTest(**item) for item in data]
+
+    except json.JSONDecodeError:
+        raise SystemExit(f"ERROR: Invalid JSON in file: {filename}")
+    except FileNotFoundError:
+        raise SystemExit("ERROR: File Not Found.")
