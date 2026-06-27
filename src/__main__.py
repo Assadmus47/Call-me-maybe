b33c7cc30234ecdf3db8bf06da8125cf5300819d
Assadmus47 <assadkac47@gmail.com>
2026-06-27 03:34:18 +0200

    fix: correct functions_definition default path

--- src/__main__.py
+++ src/__main__.py
@@ -4,12 +4,11 @@ from src.file_parser import parse_function_file, parse_calling_function_file, wr
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--functions_definition", default="data/input/function_definition.json")
+parser.add_argument("--functions_definition", default="data/input/functions_definition.json")
 parser.add_argument("--input", default="data/input/function_calling_tests.json")
 parser.add_argument("--output", default="data/output/function_calls.json")
 
 args = parser.parse_args()
-
 functions = parse_function_file(args.functions_definition)
 tests = parse_calling_function_file(args.input)
 write_output_file(args.output, [])
